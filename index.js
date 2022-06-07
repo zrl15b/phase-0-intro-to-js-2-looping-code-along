61ad74625761fdef34b0654f94df6338563205d4
Zachary <zacharylampi96@gmail.com>
2022-06-07 16:37:37 -0400

    commit tree 2da9d790fab5daebe5b7e8deb31476e5be7cd8f6

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const card = []
+function writeCards(name,event) {
+  for (let i = 0; i < name.length; i++) {
+    card.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    debugger;
+    }      
+    return card;
+     }
+    
+function countDown() {
+  let number=10
+  while (number >= 0) 
+  console.log(number--)
+
+} 
